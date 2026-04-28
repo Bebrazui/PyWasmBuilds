897fcbb33040f4c5d4bb5d5826063da9ffdcc39f
bebrazui <big.reiks@gmail.com>
2026-04-28 21:11:56 +0500

    fix: move C source to repo file, clean workflow yaml

---
+++ c-extensions/testmodule.c
@@ -0,0 +1,26 @@
+#define PY_SSIZE_T_CLEAN
+#include "Python.h"
+
+static PyObject* test_hello(PyObject* self, PyObject* args) {
+    return PyUnicode_FromString("Hello from C-extension WASM!");
+}
+
+static PyObject* test_add(PyObject* self, PyObject* args) {
+    long a, b;
+    if (!PyArg_ParseTuple(args, "ll", &a, &b)) return NULL;
+    return PyLong_FromLong(a + b);
+}
+
+static PyMethodDef TestMethods[] = {
+    {"hello", test_hello, METH_NOARGS,   "Return hello string"},
+    {"add",   test_add,   METH_VARARGS,  "Add two numbers"},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef testmodule = {
+    PyModuleDef_HEAD_INIT, "_testcext", NULL, -1, TestMethods
+};
+
+PyMODINIT_FUNC PyInit__testcext(void) {
+    return PyModule_Create(&testmodule);
+}
